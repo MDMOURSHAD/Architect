71a4823d218a9eada9b29dcd8cc228a3f1166358
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-08-21 16:19:08 +0600

    Add files via upload

---
+++ assests/js/main.js
@@ -0,0 +1,104 @@
+// Preloader 
+function load(){
+
+	var loading = document.getElementsByClassName('preloader');
+
+	loading[0].style.display = "none";
+}
+
+// MOBILE MENU //
+const menuIcon = document.querySelector('.hamburger-menu');
+const navlist = document.querySelector('.nav-list');
+
+menuIcon.addEventListener("click", () => {
+
+  menuIcon.classList.toggle('change');
+  navlist.classList.toggle('slide-menu');
+  
+});
+
+function myFunction(){
+
+  menuIcon.classList.remove('change');
+  navlist.classList.remove('slide-menu');
+  
+}
+
+$(document).ready(function(){
+	
+	// Sticky Menu
+	$(window).scroll(function(){
+
+		if($(this).scrollTop() > 100){
+			$('.nav').addClass('sticky');
+		}
+
+		else{
+			$('.nav').removeClass('sticky');
+		}
+
+	});
+	
+	// Scroll to Top
+	$(window).scroll(function(){
+
+		if($(this).scrollTop() > 100){
+			$('.scroll-top').fadeIn();
+		}
+
+		else{
+			$('.scroll-top').fadeOut();
+		}
+
+	});
+
+	// // Counter UP
+	// $('.counter').counterUp({
+ //    delay: 10,
+ //    time: 1000
+	// });//
+
+  //Latest Project Slider 
+	var partialView = $('#partial-view').partialViewSlider({
+
+  // 70%
+  width: 50,
+
+  // shows controls
+  controls: true,
+
+  // inside, outside, neighbors
+  controlsPosition: 'inside', 
+
+  // shows background
+  backdrop: true,
+
+  // shows pagination dots
+  dots: false,
+
+  // enable autoplay
+  auto: true,
+
+  // transition speed in ms
+  transitionSpeed: 500,
+
+  // autoplay delay in ms
+  delay: 3000,
+
+  // enable pause on hover
+  pauseOnHover: false,
+
+  // enable keyboard interactions
+  keyboard: true,
+
+  // enable perspective mode
+  perspective: false,
+
+  // custom next/prev arrows
+  prevHtml: '<i class="icon-arrow-circle-left-solid"></i>',
+  nextHtml: '<i class="icon-arrow-circle-right-solid"></i>'
+  
+});
+
+
+});
